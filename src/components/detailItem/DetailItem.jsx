79f21a9610964d938aa0aea95ff6f44d86285351
VaninaLopetegui <vaninalopetegui@gmail.com>
2025-01-10 21:58:40 -0300

    se siguió página detailItem y se agregaron botones de compra, aún sin realizar responsive

--- src/components/detailItem/DetailItem.jsx
+++ src/components/detailItem/DetailItem.jsx
@@ -1,6 +1,8 @@
 import { productos } from "../../data/productos";
 import { useParams } from "react-router-dom";
 import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
+import Dropdown from 'react-bootstrap/Dropdown';
 import "./detailItem.css"
 
 export const DetailItem = () => {
@@ -10,14 +12,31 @@ export const DetailItem = () => {
         return <h2>Este producto no existe</h2>
     }
     return (
-        <div className="productDetail container">
-            <div>
-                <Image src={productSelect.img} fluid/>
-            </div>
-            <div className="productDetailInfo d-flex justify-content-start flex-column">
-                <h5>Nombre de producto: <span>{productSelect.nombre}</span></h5>
-                <h5>Precio por unidad: <span>US$ {productSelect.precio}</span></h5>
-                <h5>Descripcion de producto: <span>{productSelect.descripcion}</span></h5>
+        <div className="contenedorProductDetail d-flex justify-content-center align-items-center">
+            <div className="productDetail w-75 justify-content-center align-items-center flex-row">
+                <div className="productDetailImg">
+                    <Image src={productSelect.img} fluid/>
+                </div>
+                <div className="productDetailInfo d-flex flex-column mx-3">
+                    <h6>DREAM TAPESTRY</h6>
+                    <h4>Nombre de producto: <span>'{productSelect.nombre}'</span></h4>
+                    <h4>Medidas: <span>'{productSelect.medidas}'</span></h4>
+                    <h4>Precio por unidad: <span>US$ {productSelect.precio}</span></h4>
+                    <h4>Descripción de producto: <span>'{productSelect.descripcion}'</span></h4>
+                    <h5>✔ Se aceptan cambios y devoluciones</h5>
+                    <div>
+                        <Dropdown className="mb-3">
+                            <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                1 unidad
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item>2 unidades</Dropdown.Item>
+                                <Dropdown.Item>3 unidades</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                        <Button>Agregar al carrito</Button>
+                    </div>
+                </div>
             </div>
         </div>
     )
